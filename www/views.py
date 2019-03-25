311e3f734f63ee5aa628e20bad313daa09a6e807
fsdamp <fsdamp@gmail.com>
2019-03-25 17:51:36 +0500

    Updated ajax query "/ajax/login/"

--- www/views.py
+++ www/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -16,11 +17,24 @@ class AjaxQuery:
 
     @staticmethod
     def login(request):
-        # if request.method == 'GET':
-        data = {
-            'ok': 'working!'
-        }
-        return JsonResponse(data)
+        if request.method == 'GET':
+            username = request.GET.get('username', None)
+            password = request.GET.get('password', None)
+
+            if username is not None and password is not None:
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        return JsonResponse({'ok': 'Успешная авторизация'})
+                    else:
+                        return JsonResponse({'error': 'Ваш аккаунт заблокирован'})
+                else:
+                    return JsonResponse({'error': 'Неверный логин или пароль'})
+            else:
+                return JsonResponse({'error': 'Пустой логин или пароль'})
+        else:
+            return JsonResponse({'error': 'Неверный запрос'})
 
     def register(self, request):
         pass
